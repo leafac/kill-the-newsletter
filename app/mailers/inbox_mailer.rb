0e2b622a719c41583dee7fc7e07d9ec86564d533
Leandro Facchinetti <me@leafac.com>
2019-02-05 08:19:58 -0500

    Biggest hammer

--- app/mailers/inbox_mailer.rb
+++ app/mailers/inbox_mailer.rb
@@ -9,14 +9,16 @@ class InboxMailer < ApplicationMailer
     part = email.html_part || email.text_part || email
     body = part.body.decoded
                     .force_encoding(part.content_type_parameters.fetch('charset', 'binary').strip)
-                    .encode("UTF-8", invalid: :replace, undef: :replace, replace: "")
+                    .encode('UTF-8', invalid: :replace, undef: :replace, replace: '')
     entry = Entry.new(
-      title: email.subject,
-      author: email.envelope_from,
-      content: body,
+      title: fix_encoding(email.subject),
+      author: fix_encoding(email.envelope_from),
+      content: fix_encoding(body),
       content_type: part.content_type =~ /html/ ? 'html' : 'text'
     )
     entry_string = ApplicationController.renderer.new.render 'entries/_entry', locals: { entry: entry }
+    feed_string = fix_encoding feed_string
+    entry_string = fix_encoding entry_string
     updated_feed_string = feed_string.sub /<updated>.*?<\/updated>/, entry_string
     if updated_feed_string.bytesize > MAXIMUM_FEED_SIZE
       updated_feed_string = updated_feed_string.byteslice 0, MAXIMUM_FEED_SIZE
@@ -29,4 +31,14 @@ class InboxMailer < ApplicationMailer
     logger.fatal e.backtrace.join("\n")
     raise e
   end
+
+  private
+
+    def fix_encoding string
+      if string.valid_encoding?
+        string
+      else
+        string.force_encoding('binary').encode('UTF-8', invalid: :replace, undef: :replace, replace: '')
+      end
+    end
 end
