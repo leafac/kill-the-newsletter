30e374ae294373bb5aa241536c050ccd33d5a28f
Leandro Facchinetti <me@leafac.com>
2019-06-13 16:12:54 -0400

    Add feeds

--- kill-the-newsletter.rb
+++ kill-the-newsletter.rb
@@ -5,8 +5,18 @@ get "/" do
 end
 
 post "/" do
-  @name = Rack::Utils.escape_html(params.fetch("name") { halt 400 })
-  @token = SecureRandom.alphanumeric(20).downcase
-  File.write "public/feeds/#{@token}.xml", "TODO: THE FEED"
+  @name = params.fetch("name") { halt 400 }
+  @token = fresh_token
+  File.write "public/feeds/#{@token}.xml", erb(:feed, layout: false)
   erb :created
+end
+
+helpers do
+  def fresh_token
+    SecureRandom.alphanumeric(20).downcase
+  end
+
+  def h text
+    Rack::Utils.escape_html text
+  end
 end
