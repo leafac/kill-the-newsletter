d22e0016d6bef14eb76a0e44919d8552e3795edc
Leandro Facchinetti <me@leafac.com>
2019-06-13 15:31:26 -0400

    Logic

--- kill-the-newsletter.rb
+++ kill-the-newsletter.rb
@@ -1,2 +1,12 @@
 require "sinatra"
 
+get "/" do
+  erb :new
+end
+
+post "/" do
+  @name = params.fetch("name") { halt 400 }
+  @token = SecureRandom.alphanumeric(20).downcase
+  File.write "public/feeds/#{@token}.xml", "TODO: THE FEED"
+  erb :created
+end
