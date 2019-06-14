9729e043d2d914d3db972ab117dc3de7fa25b3cd
Leandro Facchinetti <me@leafac.com>
2019-06-14 11:57:09 -0400

    Simplify stuff

--- email_handler.rb
+++ email_handler.rb
@@ -2,30 +2,40 @@ require "mail"
 require "nokogiri"
 require "./server"
 
-Sinatra::Application.new!.instance_eval do
-  # mail = Mail.new STDIN.read
-  # token = Mail::Address.new(mail.smtp_envelope_to.first).local # TODO: Remove ‘.first’
-  # # TODO: Check that ‘token’ isn’t malicious and follows the format
-  # # feed = File.read File.expand_path("../public/feeds/#{token}.xml", __FILE__) rescue return
-  # part = mail.multipart? ? mail.html_part || mail.text_part : mail
-  # # part.content_type_parameters => {'charset' => 'ISO-8859-1'}
-  # entry = 
-  #   erb :entry, layout: false, locals: {
-  #     title: mail.subject,
-  #     author: mail.smtp_envelope_from, # mail.from.addresses (LIST) OR mail.sender.address OR mail.envelope_from OR mail.smtp_envelope_from OR mail.reply_to (LIST)
-  #     content_type: part.text? ? "text" : "html",
-  #   } do
-  #     part.decoded
-  #   end
-  # end
+# mail = Mail.new STDIN.read
+# token = Mail::Address.new(mail.smtp_envelope_to.first).local # TODO: Remove ‘.first’
+# # TODO: Check that ‘token’ isn’t malicious and follows the format
+# # feed = File.read File.expand_path("../public/feeds/#{token}.xml", __FILE__) rescue return
+# part = mail.multipart? ? mail.html_part || mail.text_part : mail
+# # part.content_type_parameters => {'charset' => 'ISO-8859-1'}
+# entry = 
+#   erb :entry, layout: false, locals: {
+#     title: mail.subject,
+#     author: mail.smtp_envelope_from, # mail.from.addresses (LIST) OR mail.sender.address OR mail.envelope_from OR mail.smtp_envelope_from OR mail.reply_to (LIST)
+#     content_type: part.text? ? "text" : "html",
+#   } do
+#     part.decoded
+#   end
+# end
 
-  # puts entry
+# puts entry
 
-  x = Nokogiri::XML(File.read("public/feeds/7dkg97z4zr9lqvwdwdf9.xml"), nil, "UTF-8") { |config| config.strict }
-  x.at_css("updated").replace(erb(:entry, layout: false, locals: { title: "h<i>", author: "te<s>t", content_type: "text" }) { "bund<i>nha" })
-  x.at_css("entry:last-of-type").remove until x.to_s.length <= 500_000
-  puts x
-end
+feed = Nokogiri::XML(File.read("public/feeds/7dkg97z4zr9lqvwdwdf9.xml"), nil, "UTF-8") { |config| config.strict }
+feed.at_css("updated").replace(
+  Sinatra::Application.new.helpers.erb(
+    :entry,
+    layout: false,
+    locals: {
+      title: mail.subject,
+      author: mail.smtp_envelope_from, # mail.from.addresses (LIST) OR mail.sender.address OR mail.envelope_from OR mail.smtp_envelope_from OR mail.reply_to (LIST)
+      content_type: part.text? ? "text" : "html",
+      content: part.decoded,
+    }
+  )
+)
+feed.at_css("entry:last-of-type").remove until feed.to_s.length <= 500_000
+puts x
+
 
 # class InboxMailer < ApplicationMailer
 #   MAXIMUM_FEED_SIZE = 500_000
