a3c7573096086aee7b9e45403bd079d953530d30
Leandro Facchinetti <me@leafac.com>
2019-06-14 21:13:47 -0400

    [ci skip] Don’t use after_success

--- user_data.sh
+++ user_data.sh
@@ -10,3 +10,5 @@ ufw enable
 rsync -av --chown=kill-the-newsletter:kill-the-newsletter ~/.ssh /home/kill-the-newsletter
 
 # https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/#generating-a-new-ssh-key
+
+# https://github.com/do-community/automated-setups/blob/master/Ubuntu-18.04/initial_server_setup.sh
