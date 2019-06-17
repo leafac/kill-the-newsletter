ba59b1b86b8212a4d9bd53a400d51dfa654367f5
Leandro Facchinetti <me@leafac.com>
2019-06-17 08:27:25 -0400

    Finish mail handler

--- mail_handler.rb
+++ mail_handler.rb
@@ -1,32 +1,32 @@
 require "mail"
 require "nokogiri"
+require "logger"
 require "./server"
 
-mail = Mail.new STDIN.read
-token = Mail::Address.new(mail[:envelope_to].decoded).local.downcase
-return unless token =~ /\A[a-z0-9]{20,100}\z/
-feed_path = File.expand_path "../public/feeds/#{token}.xml", __FILE__
-return unless File.file? feed_path
-feed = Nokogiri::XML(File.read(feed_path)) { |config| config.strict.noblanks }
-part = mail.html_part || mail.text_part || mail
-feed.at_css("updated").replace(
-  Sinatra::Application.new.helpers.erb(
-    :entry,
-    layout: false,
-    locals: {
-      title: mail.subject,
-      author: mail[:from]&.decoded || mail.envelope_from,
-      content_type: part.content_type =~ /html/ ? "html" : "text",
-      content: part.decoded,
-    }
+logger = Logger.new "mail_handler.log"
+begin
+  mail = Mail.new STDIN.read
+  token = Mail::Address.new(mail[:envelope_to].decoded).local.downcase
+  return unless token =~ /\A[a-z0-9]{20,100}\z/
+  feed_path = File.expand_path "../public/feeds/#{token}.xml", __FILE__
+  return unless File.file? feed_path
+  feed = Nokogiri::XML(File.read(feed_path)) { |config| config.strict.noblanks }
+  part = mail.html_part || mail.text_part || mail
+  feed.at_css("updated").replace(
+    Sinatra::Application.new.helpers.erb(
+      :entry,
+      layout: false,
+      locals: {
+        title: mail.subject,
+        author: mail[:from]&.decoded || mail.envelope_from,
+        content_type: part.content_type =~ /html/ ? "html" : "text",
+        content: part.decoded,
+      }
+    )
   )
-)
-feed.at_css("entry:last-of-type").remove until feed.to_xml.length <= 500_000
-File.write feed_path, feed.to_xml
-
-# LOGGER
-#   rescue => e
-#     logger.fatal e.message
-#     logger.fatal e.backtrace.join("\n")
-#     raise e
-#   end
+  feed.at_css("entry:last-of-type").remove until feed.to_xml.length <= 500_000
+  File.write feed_path, feed.to_xml
+rescue => e
+  logger.fatal [e.message, *e.backtrace].join("\n")
+  abort
+end
