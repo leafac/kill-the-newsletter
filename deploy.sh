675e76bf31172ccc512c6bd9ac0d7425920c244a
Leandro Facchinetti <me@leafac.com>
2019-06-19 21:59:10 -0400

    Change hostname

--- deploy.sh
+++ deploy.sh
@@ -4,18 +4,18 @@ set -euxo pipefail
 
 ssh-add
 
-ssh -tA root@www.kill-the-newsletter.com 'systemctl stop server caddy exim'
+ssh -tA root@kill-the-newsletter.com 'systemctl stop server caddy exim'
 
-ssh -tA kill-the-newsletter@www.kill-the-newsletter.com 'cd www.kill-the-newsletter.com && git pull origin master'
+ssh -tA kill-the-newsletter@kill-the-newsletter.com 'cd www.kill-the-newsletter.com && git pull origin master'
 
-ssh -tA kill-the-newsletter@www.kill-the-newsletter.com 'cd www.kill-the-newsletter.com && bash -ic "bundle install"'
+ssh -tA kill-the-newsletter@kill-the-newsletter.com 'cd www.kill-the-newsletter.com && bash -ic "bundle install"'
 
-ssh -tA root@www.kill-the-newsletter.com 'rsync -av --chown root:root ~kill-the-newsletter/www.kill-the-newsletter.com/exim.conf /home/linuxbrew/.linuxbrew/etc/exim.conf'
+ssh -tA root@kill-the-newsletter.com 'rsync -av --chown root:root ~kill-the-newsletter/www.kill-the-newsletter.com/exim.conf /home/linuxbrew/.linuxbrew/etc/exim.conf'
 
-ssh -tA root@www.kill-the-newsletter.com 'rsync -av --chown root:root ~kill-the-newsletter/www.kill-the-newsletter.com/server.service /etc/systemd/system/server.service'
-ssh -tA root@www.kill-the-newsletter.com 'rsync -av --chown root:root ~kill-the-newsletter/www.kill-the-newsletter.com/caddy.service /etc/systemd/system/caddy.service'
-ssh -tA root@www.kill-the-newsletter.com 'rsync -av --chown root:root ~kill-the-newsletter/www.kill-the-newsletter.com/exim.service /etc/systemd/system/exim.service'
+ssh -tA root@kill-the-newsletter.com 'rsync -av --chown root:root ~kill-the-newsletter/www.kill-the-newsletter.com/server.service /etc/systemd/system/server.service'
+ssh -tA root@kill-the-newsletter.com 'rsync -av --chown root:root ~kill-the-newsletter/www.kill-the-newsletter.com/caddy.service /etc/systemd/system/caddy.service'
+ssh -tA root@kill-the-newsletter.com 'rsync -av --chown root:root ~kill-the-newsletter/www.kill-the-newsletter.com/exim.service /etc/systemd/system/exim.service'
 
-ssh -tA root@www.kill-the-newsletter.com 'systemctl daemon-reload'
-ssh -tA root@www.kill-the-newsletter.com 'systemctl start server caddy exim'
-ssh -tA root@www.kill-the-newsletter.com 'systemctl enable server caddy exim'
+ssh -tA root@kill-the-newsletter.com 'systemctl daemon-reload'
+ssh -tA root@kill-the-newsletter.com 'systemctl start server caddy exim'
+ssh -tA root@kill-the-newsletter.com 'systemctl enable server caddy exim'
