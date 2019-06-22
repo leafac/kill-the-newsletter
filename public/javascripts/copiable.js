e0711c404a58108e6031ca208b72d2c56f3c6ec9
Leandro Facchinetti <me@leafac.com>
2019-06-22 09:21:32 -0400

    Better styles

--- public/javascripts/copiable.js
+++ public/javascripts/copiable.js
@@ -1,6 +1,6 @@
 document.querySelectorAll(".copiable").forEach(element => {
   element.outerHTML = `
-  <input type="text" value="${element.innerText}" onclick="this.select();" size="65" readonly>
+  <input type="text" value="${element.innerText}" onclick="this.select();" size="65" readonly style="text-align: center;">
   <button onclick="this.previousElementSibling.select(); document.execCommand('copy'); this.innerText = 'Done';">Copy</button>
 `;
 });
