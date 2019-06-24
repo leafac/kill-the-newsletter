e99763af374f1dabe6ba54373ae2f541a488c074
Leandro Facchinetti <me@leafac.com>
2019-06-24 18:09:17 -0400

    Add lock and atomic write

--- mail_handler.rb
+++ mail_handler.rb
@@ -1,5 +1,7 @@
 require "mail"
 require "nokogiri"
+require "tempfile"
+require "fileutils"
 require "logger"
 require "./server"
 
@@ -12,22 +14,33 @@ begin
   return unless token =~ /\A[a-z0-9]{20,100}\z/
   feed_path = File.expand_path "../public/feeds/#{token}.xml", __FILE__
   return unless File.file? feed_path
-  feed = Nokogiri::XML(File.read(feed_path).scrub) { |config| config.strict.noblanks }
-  part = mail.html_part || mail.text_part || mail
-  feed.at_css("updated").replace(
-    Sinatra::Application.new.helpers.erb(
-      :entry,
-      layout: false,
-      locals: {
-        title: mail.subject,
-        author: mail[:from]&.decoded || mail.envelope_from,
-        content_type: part.content_type =~ /html/ ? "html" : "text",
-        content: part.decoded,
-      }
-    ).scrub
-  )
-  feed.at_css("entry:last-of-type").remove until feed.to_xml.length <= 500_000
-  File.write feed_path, feed.to_xml.scrub
+  File.open feed_path, "r+" do |feed_file_handler|
+    unless feed_file_handler.flock File::LOCK_EX | File::LOCK_NB
+      logger.info "Can’t obtain lock for ‘#{feed_path}’"
+      abort
+    end
+    feed = Nokogiri::XML(feed_file_handler.read.scrub) { |config| config.strict.noblanks }
+    part = mail.html_part || mail.text_part || mail
+    feed.at_css("updated").replace(
+      Sinatra::Application.new.helpers.erb(
+        :entry,
+        layout: false,
+        locals: {
+          title: mail.subject,
+          author: mail[:from]&.decoded || mail.envelope_from,
+          content_type: part.content_type =~ /html/ ? "html" : "text",
+          content: part.decoded,
+        }
+      ).scrub
+    )
+    feed.at_css("entry:last-of-type").remove until feed.to_xml.length <= 500_000
+    Tempfile.create do |feed_temp|
+      feed_temp.write feed.to_xml.scrub
+      FileUtils.mv feed_temp.path, feed_path
+    end
+  ensure
+    feed_file_handler.flock File::LOCK_UN
+  end
 rescue => e
   logger.fatal [e.message, *e.backtrace, mail_string].join("\n")
   abort
