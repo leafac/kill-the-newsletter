8bedda75d819e847395c2fd9fe82b480a4a5a474
Leandro Facchinetti <git@leafac.com>
2020-03-18 20:17:55 -0400

    .

--- src/server.tsx
+++ src/server.tsx
@@ -13,31 +13,27 @@ import {
 } from "./components";
 import fs from "fs";
 
-const webApp = express();
-
-webApp.use(express.static("static"));
-webApp.use(express.urlencoded({ extended: true }));
-
-webApp.get("/", (req, res) =>
-  res.send(
-    renderHtml(
-      <Layout>
-        <Form></Form>
-      </Layout>
+export const webServer = express()
+  .use(express.static("static"))
+  .use(express.urlencoded({ extended: true }))
+  .get("/", (req, res) =>
+    res.send(
+      renderHtml(
+        <Layout>
+          <Form></Form>
+        </Layout>
+      )
     )
   )
-);
-
-webApp.post("/", (req, res) => {
-  const inbox: Inbox = { name: req.body.name, token: newToken() };
-  fs.writeFileSync(feedPath(inbox.token), renderXml(Feed(inbox)));
-  res.send(
-    renderHtml(
-      <Layout>
-        <Created inbox={inbox}></Created>
-      </Layout>
-    )
-  );
-});
-
-export const webServer = webApp.listen(8443);
+  .post("/", (req, res) => {
+    const inbox: Inbox = { name: req.body.name, token: newToken() };
+    fs.writeFileSync(feedPath(inbox.token), renderXml(Feed(inbox)));
+    res.send(
+      renderHtml(
+        <Layout>
+          <Created inbox={inbox}></Created>
+        </Layout>
+      )
+    );
+  })
+  .listen(8443);
