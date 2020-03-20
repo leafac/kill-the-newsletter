8d08525766134c708cc89efa1cf4ffc201402214
Leandro Facchinetti <git@leafac.com>
2020-03-20 01:54:55 -0400

    .

--- src/tasks.ts
+++
@@ -1,5 +0,0 @@
-// execInFolder(`git pull origin master`);
-// execInFolder(`npm ci`);
-// TODO: rsync
-// TODO: $ pm2 startup OR $ pm2 save
-// TODO: $ pm2 start env.js --watch --ignore-watch="node_modules"
