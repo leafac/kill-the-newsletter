2138af838d9b829145f24d789c32ca32d4bb671c
Leandro Facchinetti <git@leafac.com>
2020-03-21 12:05:51 -0400

    .

---
+++ src/verify.tsx
@@ -0,0 +1,17 @@
+import xml2js from "xml2js";
+import fs from "fs";
+
+(async () => {
+  for (const feed of fs
+    .readdirSync("static/feeds")
+    .filter(file => !file.startsWith(".")))
+    try {
+      const xml = await new xml2js.Parser().parseStringPromise(
+        fs.readFileSync(`static/feeds/${feed}`, "utf8")
+      );
+      if (xml?.feed?.updated === undefined)
+        throw new Error("Can’t find xml.feed.updated");
+    } catch (error) {
+      console.log(`Error for ${feed}: ${error}`);
+    }
+})();
